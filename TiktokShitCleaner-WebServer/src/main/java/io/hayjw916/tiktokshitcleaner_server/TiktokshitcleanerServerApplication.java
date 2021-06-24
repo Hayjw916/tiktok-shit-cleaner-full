e95925752ae0f253915caa3bddb596e27fcce9ab
Hayjw916 <hayjw916@gmail.com>
2021-06-23 20:56:11 -0500

    Added the server directory

---
+++ TiktokShitCleaner-WebServer/src/main/java/io/hayjw916/tiktokshitcleaner_server/TiktokshitcleanerServerApplication.java
@@ -0,0 +1,13 @@
+package io.hayjw916.tiktokshitcleaner_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiktokshitcleanerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiktokshitcleanerServerApplication.class, args);
+	}
+
+}
---
+++ TiktokShitCleaner-WebServer/src/test/java/io/hayjw916/tiktokshitcleaner_server/TiktokshitcleanerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hayjw916.tiktokshitcleaner_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiktokshitcleanerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
