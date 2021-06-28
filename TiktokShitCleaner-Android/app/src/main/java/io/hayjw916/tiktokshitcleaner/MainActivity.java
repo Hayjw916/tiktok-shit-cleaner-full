b0aca737c10b408a1de06db598a16fd44249e492
Hayden Webber <hayjw916@gmail.com>
2021-06-27 23:39:07 -0500

    Added a video view to see the video when i selected it

--- TiktokShitCleaner-Android/app/src/main/java/io/hayjw916/tiktokshitcleaner/MainActivity.java
+++ TiktokShitCleaner-Android/app/src/main/java/io/hayjw916/tiktokshitcleaner/MainActivity.java
@@ -1,25 +1,29 @@
 package io.hayjw916.tiktokshitcleaner;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button tiktokButton;
-    private Button viewTiktokButton;
+    private VideoView videoView;
     private TextView decisionText;
+    private Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tiktokButton = findViewById(R.id.tiktokButton);
-        viewTiktokButton = findViewById(R.id.viewTiktokButton);
+        videoView = findViewById(R.id.videoView);
         decisionText = findViewById(R.id.decisionText);
 
         tiktokButton.setOnClickListener(new View.OnClickListener() {
@@ -32,5 +36,20 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == 1 && requestCode == RESULT_OK && data.getData() != null) {
+            uri = data.getData();
+            try {
+                if (uri.toString().contains("image")) {
+                    decisionText.setText("This video contains an image!");
+                }
+                videoView.setVideoPath(uri.toString());
+                videoView.start();
+            } catch (Exception e) {
 
+            }
+        }
+    }
 }
