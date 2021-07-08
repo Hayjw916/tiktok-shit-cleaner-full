8aa910c14b927bafa08dd2dc47f204389a188b45
Hayjw916 <hayjw916@gmail.com>
2021-07-08 12:37:19 -0500

    Get storage permissions and save the extracted mp3 file to the data folder

--- TiktokShitCleaner-Android/app/src/main/java/io/hayjw916/tiktokshitcleaner/MainActivity.java
+++ TiktokShitCleaner-Android/app/src/main/java/io/hayjw916/tiktokshitcleaner/MainActivity.java
@@ -1,10 +1,15 @@
 package io.hayjw916.tiktokshitcleaner;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,6 +18,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import io.hayjw916.tiktokshitcleaner.util.AudioExtractor;
@@ -22,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     private Button tiktokButton;
     private TextView decisionText;
     private Uri uri;
+
+    private final int STORAGE_PERMISSION_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
         tiktokButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE);
+                checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE);
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType("video/mp4");
                 startActivityForResult(intent, 1);
@@ -50,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
                     decisionText.setText("This video contains an image!");
                 } else {
                     decisionText.setText("You got a video");
-                    new AudioExtractor().genVideoUsingMuxer(RealPathUtil.getRealPath(getApplicationContext(), uri), "bruh.mp3", -1, -1, true, false);
+                        new AudioExtractor().genVideoUsingMuxer(RealPathUtil.getRealPath(getApplicationContext(), uri),   getDataDir(getApplicationContext())+ "/bruh.mp3", -1, -1, true, false);
                     System.out.println("Got video!");
                 }
             } catch (Exception e) {
@@ -61,26 +71,28 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * Didn't want to make my own, so i kinda copied it.
-     * apparently it's a headache for a lot of android developers*/
-    private String getRealPathFromURI(Context context, Uri contentUri) {
-        Cursor cursor = null;
-        try {
-            String[] proj = { MediaStore.Video.Media.DATA };
-            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
-        } catch (Exception e) {
-            Log.e(MainActivity.class.getCanonicalName(), "getRealPathFromURI Exception : " + e.toString());
-            return "";
-        } finally {
-            if (cursor != null) {
-                cursor.close();
+    public void checkPermission(String permission, int requestCode) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_DENIED) {
+            // Requesting permission
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] { permission }, requestCode);
+        } else {
+            Toast.makeText(MainActivity.this, "Permission already granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "Storage Permission Granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Storage Permission Denied", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    
+    public String getDataDir(Context context) throws Exception {
+        return context.getPackageManager().getPackageInfo(context.getPackageName(), 0).applicationInfo.dataDir;
+    }
 }
