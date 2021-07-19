cd9f45c6e880cf99df42296e0fb2c0f9ca416d2d
Hayjw916 <hayjw916@gmail.com>
2021-07-18 21:14:51 -0500

    Added all of the fields to the SongDB class

--- TiktokShitCleaner-Server/src/main/java/io/hayjw916/tiktokshitcleaner/server/model/SongDB.java
+++ TiktokShitCleaner-Server/src/main/java/io/hayjw916/tiktokshitcleaner/server/model/SongDB.java
@@ -1,6 +1,12 @@
 package io.hayjw916.tiktokshitcleaner.server.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Lob;
 import javax.persistence.Table;
 
 /**
@@ -10,4 +16,28 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "songs")
 public class SongDB {
+
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    @Getter
+    @Setter
+    @Lob
+    private byte[] data;
+
+    public SongDB() { }
+
+    public SongDB(String name, String id, byte[] data) {
+        this.name = name;
+        this.id = id;
+        this.data = data;
+    }
+
 }
