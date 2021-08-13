fcd7a6b8b6a3d932a9b3967ccb5711e4c4eb5e8c
Hayden <hayjw916@gmail.com>
2021-08-13 17:53:05 -0500

    Added javadoc and songPath value

--- TiktokShitCleaner-Server/src/main/java/io/hayjw916/tiktokshitcleaner/server/service/SongService.java
+++ TiktokShitCleaner-Server/src/main/java/io/hayjw916/tiktokshitcleaner/server/service/SongService.java
@@ -1,7 +1,23 @@
 package io.hayjw916.tiktokshitcleaner.server.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+/**
+ * The purpose of the SongService is to create a folder that will store
+ * all of the songs on the computer as well as storing the info for the songs in
+ * a database.
+ *
+ * It also has a convert method which converts mp3 files to wav files since we need to be using
+ * .wav instead of .mp3.
+ *
+ * 
+ * */
 @Service
 public class SongService {
+
+    @Value("${song.path}")
+    private String songPath;
+
+
 }
