20aa39ebd0027fb9a8a0faafe9f349c86ccdd23d
Hayden <hayjw916@gmail.com>
2021-08-13 18:53:46 -0500

    Add a submission.path value

--- TiktokShitCleaner-Server/src/main/java/io/hayjw916/tiktokshitcleaner/server/service/SubmissionService.java
+++ TiktokShitCleaner-Server/src/main/java/io/hayjw916/tiktokshitcleaner/server/service/SubmissionService.java
@@ -1,7 +1,17 @@
 package io.hayjw916.tiktokshitcleaner.server.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+/**
+ * This service handles storing the submission files and converting them.
+ * */
 @Service
 public class SubmissionService extends SongService {
+
+    /**This value comes from whatever submission.path is in the application.properties file*/
+    @Value("${submission.path}")
+    private String submissionPath;
 }
