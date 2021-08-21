02e54b7d8cc9cea2b8ca3542a66e3fa9abdb98e3
Hayden Webber <hayjw916@gmail.com>
2021-08-21 12:26:35 -0500

    Added listSong, listAllSongs, and deleteSongs methods

--- TiktokShitCleaner-Server/src/main/java/io/hayjw916/tiktokshitcleaner/server/service/SongService.java
+++ TiktokShitCleaner-Server/src/main/java/io/hayjw916/tiktokshitcleaner/server/service/SongService.java
@@ -1,24 +1,25 @@
 package io.hayjw916.tiktokshitcleaner.server.service;
 
-import io.hayjw916.tiktokshitcleaner.server.model.SongModel;
-import io.hayjw916.tiktokshitcleaner.server.repo.SongDBRepo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.CopyOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * The purpose of the SongService is to create a folder that will store
- * all of the songs on the computer as well as storing the info for the songs in
+ * all the songs on the computer as well as storing the info for the songs in
  * a database.
  *
  * It also has a convert method which converts mp3 files to wav files since we need to be using
@@ -29,14 +30,11 @@ import java.util.Objects;
 @Service
 public class SongService {
 
-    @Autowired
-    public SongDBRepo songDBRepo;
-
     /**Same value as song.path in the application.properties file*/
     @Value("${song.path}")
     private String songPath;
 
-    /**This method creates a folder based on the the song.path value is in application.properties*/
+    /**This method creates a folder based on the song.path value is in application.properties*/
     public void createSongPath() {
         try {
             Files.createDirectories(Path.of(songPath));
@@ -60,5 +58,38 @@ public class SongService {
         }
     }
 
+    public Resource loadSong(String fileName) {
+        try {
+            Path file = Paths.get(songPath).resolve(fileName);
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new RuntimeException("File does not exist");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("There was an error: " + e.getMessage());
+        }
+    }
+
+    public List<Path> loadAllSongs() {
+        try {
+            Path root = Paths.get(songPath);
+            if (Files.exists(root)) {
+                return Files.walk(root, 1)
+                        .filter(path -> !path.equals(root))
+                        .collect(Collectors.toList());
+            }
+
+            return Collections.emptyList();
+        } catch(IOException e) {
+            throw new RuntimeException("Unable to list the files");
+        }
+    }
+
+    public void deleteAllSongs() {
+        FileSystemUtils.deleteRecursively(Paths.get(songPath).toFile());
+    }
 
 }
